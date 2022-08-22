7005a38dd5974452be112352592c76dc15f9fa66
Max Adulyanukosol <namax7@gmail.com>
2022-08-22 15:47:29 -0400

    fix: update function parameter in test

--- tests/test_convert_omop_to_cadence_csv.py
+++ tests/test_convert_omop_to_cadence_csv.py
@@ -1,15 +1,17 @@
 from omop_to_cadence_converter import convert_omop_to_cadence_csv, generate_query_csv
 from pyarrow import types
+import duckdb
 
+conn = duckdb.connect()
 
 def test_attributes_datatypes():
-	table_attributes = convert_omop_to_cadence_csv.convert_omop_to_cadence(generate_query_csv.attributes()).arrow()
+	table_attributes = convert_omop_to_cadence_csv.convert_omop_to_cadence(conn, generate_query_csv.attributes()).arrow()
 	assert types.is_string(table_attributes['variable'].type)
 	assert types.is_string(table_attributes['value'].type)
 	assert types.is_string(table_attributes['type'].type)
 	
 def test_events_datatypes():
-	table_events = convert_omop_to_cadence_csv.convert_omop_to_cadence(generate_query_csv.events()).arrow()
+	table_events = convert_omop_to_cadence_csv.convert_omop_to_cadence(conn, generate_query_csv.events()).arrow()
 	assert types.is_timestamp(table_events['date'].type)
 	assert types.is_string(table_events['codeclass'].type)
 	assert types.is_string(table_events['code'].type)
