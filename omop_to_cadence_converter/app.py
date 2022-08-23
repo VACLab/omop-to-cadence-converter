e56b83f91da1e8fe343368071a10a3fe3a7d3f15
Max Adulyanukosol <namax7@gmail.com>
2022-08-23 11:01:45 -0400

    feat: add sanity checks

--- omop_to_cadence_converter/app.py
+++ omop_to_cadence_converter/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import duckdb
 import configparser
+import altair as alt
 
 import convert_omop_to_cadence_csv
 import generate_query_csv
@@ -9,7 +10,7 @@ import generate_query_csv
 config = configparser.ConfigParser()
 config.read('config/config.ini')
 
-st.title('OMOP to CADENCE Converter')
+st.sidebar.title('OMOP to CADENCE Converter')
 
 with st.form("config"):
 	### Config ###
@@ -29,28 +30,29 @@ with st.form("config"):
 	submitted = st.form_submit_button('Run conversion')
 
 	if submitted:
-		### OMOP ###
-		st.header('OMOP')
+		### OMOP CDM ###
+		st.header('OMOP CDM')
 
+		with st.expander("See sample data"):
+			# OMOP Person Table
+			df_person = pd.read_csv(person_table_csv_path, nrows=20)
 
-		# OMOP Person Table
-		df_person = pd.read_csv(person_table_csv_path, nrows=20)
+			st.subheader('Person Table (sample)')
+			st.dataframe(df_person)
 
-		st.subheader('Person Table (sample)')
-		st.dataframe(df_person)
+			# OMOP Condition Occurrence Table
+			df_condition_occurrence = pd.read_csv(condition_occurrence_table_csv_path, nrows=20)
 
-		# OMOP Condition Occurrence Table
-		df_condition_occurrence = pd.read_csv(condition_occurrence_table_csv_path, nrows=20)
+			st.subheader('Condition Occurrence Table (sample)')
+			st.dataframe(df_condition_occurrence)
 
-		st.subheader('Condition Occurrence Table (sample)')
-		st.dataframe(df_condition_occurrence)
+			# OMOP Concept Table
+			df_concept = pd.read_csv(concept_table_csv_path, nrows=20)
 
-		# OMOP Concept Table
-		df_concept = pd.read_csv(concept_table_csv_path, nrows=20)
+			st.subheader('Concept Table (sample)')
+			st.dataframe(df_concept)
 
-		st.subheader('Concept Table (sample)')
-		st.dataframe(df_concept)
-
+		st.markdown('----------------------------------------------------')
 		### CADENCE ###
 		st.header('CADENCE')
 
@@ -72,6 +74,21 @@ with st.form("config"):
 		st.text('Summary of Attributes Table')
 		st.write(df_attributes.describe(include='all').fillna("").astype("str"))
 
+		st.altair_chart(alt.Chart(df_attributes[df_attributes['variable']=='age'], title='Age distribution').mark_bar().encode(
+			alt.X('value:Q', bin=True, title='Age'),
+			y='count()'
+		), use_container_width=True)
+
+		st.altair_chart(alt.Chart(df_attributes[df_attributes['variable']=='gender'], title='Gender distribution').mark_bar().encode(
+			alt.X('value:N', title='Gender', sort=alt.EncodingSortField(field="value", op="count", order='descending'), axis=alt.Axis(labelAngle=0)),
+			y='count()'
+		), use_container_width=True)
+
+		st.altair_chart(alt.Chart(df_attributes[df_attributes['variable']=='race'], title='Race distribution').mark_bar().encode(
+			alt.X('value:N', title='Race', sort=alt.EncodingSortField(field="value", op="count", order='descending'), axis=alt.Axis(labelAngle=0)),
+			y='count()'
+		), use_container_width=True)
+
 		# CADENCE Events Table
 		st.subheader('Events Table (sample)')
 		df_events = convert_omop_to_cadence_csv.convert_omop_to_cadence(
@@ -85,6 +102,21 @@ with st.form("config"):
 
 		st.text('Summary of Events Table')
 		st.write(df_events.describe(include='all', datetime_is_numeric=True).fillna("").astype("str"))
+
+		st.altair_chart(alt.Chart(df_events, title='Year distribution').mark_bar().encode(
+			alt.X('year(date):T', title='Year'),
+			y='count()'
+		), use_container_width=True)
+
+		st.altair_chart(alt.Chart(df_events, title='Code Class distribution').mark_bar().encode(
+			alt.X('codeclass:N', title='Code Class', axis=alt.Axis(labelAngle=0)),
+			y='count()'
+		), use_container_width=True)
+
+		st.altair_chart(alt.Chart(df_events, title='Code distribution').mark_bar().encode(
+			alt.Y('code:N', title='Code', sort=alt.EncodingSortField(field="code", op="count", order='descending'), axis=alt.Axis(labelAngle=0)),
+			x='count()'
+		), use_container_width=True)
 
 		convert_omop_to_cadence_csv.if_dir_not_exists_create(csv_export_path)
 		convert_omop_to_cadence_csv.export_as_csv(
