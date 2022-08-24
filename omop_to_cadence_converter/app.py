2ee3bd80e0799dbc3ee99590e1c9443e7d453412
Max Adulyanukosol <namax7@gmail.com>
2022-08-24 10:24:52 -0400

    feat: add postgres support on app

--- omop_to_cadence_converter/app.py
+++ omop_to_cadence_converter/app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import pandas as pd
 import duckdb
+import psycopg2
 import configparser
 import altair as alt
 
 import convert_omop_to_cadence_csv
 import generate_query_csv
+
+import convert_omop_to_cadence_postgres
+import generate_query_postgres
 
 config = configparser.ConfigParser()
 config.read('config/config.ini')
@@ -13,6 +17,14 @@ config.read('config/config.ini')
 st.sidebar.title('OMOP to CADENCE Converter')
 
 with st.form("config"):
+
+	st.sidebar.header('Data Source')
+	datasource = st.sidebar.selectbox('Please data source format', ['CSV', 'PostgreSQL'])
+	if datasource == 'PostgreSQL':
+		postgres_connection_string = st.sidebar.text_input(
+		    'PostgreSQL connection string:', config['POSTGRES']['CONNECTION_STRING']
+		)
+
 	### Config ###
 	st.sidebar.header('Config')
 	year_for_age_calculation = st.sidebar.number_input(
@@ -24,36 +36,76 @@ with st.form("config"):
 	)
 
 	st.sidebar.header('OMOP')
-	person_table_csv_path = st.sidebar.text_input('CSV path for Person table:', config['CSV']['TABLE_PERSON'])
-	condition_occurrence_table_csv_path = st.sidebar.text_input(
-	    'CSV path for Condition Occurrence table:', config['CSV']['TABLE_CONDITION_OCCURRENCE']
-	)
-	concept_table_csv_path = st.sidebar.text_input('CSV path for Concept table:', config['CSV']['TABLE_CONCEPT'])
+
+	# CSV
+	if datasource == 'CSV':
+		person_table_csv_path = st.sidebar.text_input('CSV path for Person table:', config['CSV']['TABLE_PERSON'])
+		condition_occurrence_table_csv_path = st.sidebar.text_input(
+		    'CSV path for Condition Occurrence table:', config['CSV']['TABLE_CONDITION_OCCURRENCE']
+		)
+		concept_table_csv_path = st.sidebar.text_input('CSV path for Concept table:', config['CSV']['TABLE_CONCEPT'])
+
+	# PostgreSQL
+	if datasource == 'PostgreSQL':
+		person_table_postgres = st.sidebar.text_input(
+		    'Schema.Database for Person table:', config['POSTGRES']['TABLE_PERSON']
+		)
+		condition_occurrence_table_postgres = st.sidebar.text_input(
+		    'Schema.Database for Condition Occurrence table:', config['POSTGRES']['TABLE_CONDITION_OCCURRENCE']
+		)
+		concept_table_postgres = st.sidebar.text_input(
+		    'Schema.Database for Concept table:', config['POSTGRES']['TABLE_CONCEPT']
+		)
 
 	st.sidebar.header('CADENCE')
-	csv_export_path = st.sidebar.text_input('CSV export path:', config['CSV']['EXPORT_PATH'])
+	if datasource == 'CSV':
+		csv_export_path = st.sidebar.text_input('CSV export path:', config['CSV']['EXPORT_PATH'])
+
+	if datasource == 'PostgreSQL':
+		st.sidebar.write('Output will be exported to `cadence.attributes` and `cadence.events` tables')
 
 	submitted = st.form_submit_button('Run conversion')
 
 	if submitted:
+		if datasource == 'CSV':
+			conn = duckdb.connect()
+		if datasource == 'PostgreSQL':
+			conn = psycopg2.connect(postgres_connection_string)
+			cur = conn.cursor()
+
 		### OMOP CDM ###
 		st.header('OMOP CDM')
 
 		with st.expander("See sample data"):
 			# OMOP Person Table
-			df_person = pd.read_csv(person_table_csv_path, nrows=20)
+			if datasource == 'CSV':
+				df_person = pd.read_csv(person_table_csv_path, nrows=20)
+			if datasource == 'PostgreSQL':
+				cur.execute(f'SELECT * FROM {person_table_postgres} LIMIT 20')
+				df_person = pd.DataFrame(cur.fetchall())
+				df_person.columns = [desc[0] for desc in cur.description]
 
 			st.subheader('Person Table (sample)')
 			st.dataframe(df_person)
 
 			# OMOP Condition Occurrence Table
-			df_condition_occurrence = pd.read_csv(condition_occurrence_table_csv_path, nrows=20)
+			if datasource == 'CSV':
+				df_condition_occurrence = pd.read_csv(condition_occurrence_table_csv_path, nrows=20)
+			if datasource == 'PostgreSQL':
+				cur.execute(f'SELECT * FROM {condition_occurrence_table_postgres} LIMIT 20')
+				df_condition_occurrence = pd.DataFrame(cur.fetchall())
+				df_condition_occurrence.columns = [desc[0] for desc in cur.description]
 
 			st.subheader('Condition Occurrence Table (sample)')
 			st.dataframe(df_condition_occurrence)
 
 			# OMOP Concept Table
-			df_concept = pd.read_csv(concept_table_csv_path, nrows=20)
+			if datasource == 'CSV':
+				df_concept = pd.read_csv(concept_table_csv_path, nrows=20)
+			if datasource == 'PostgreSQL':
+				cur.execute(f'SELECT * FROM {concept_table_postgres} LIMIT 20')
+				df_concept = pd.DataFrame(cur.fetchall())
+				df_concept.columns = [desc[0] for desc in cur.description]
 
 			st.subheader('Concept Table (sample)')
 			st.dataframe(df_concept)
@@ -62,19 +114,33 @@ with st.form("config"):
 		### CADENCE ###
 		st.header('CADENCE')
 
-		conn = duckdb.connect()
-
 		# CADENCE Attributes Table
 		st.subheader('Attributes Table (sample)')
 
-		df_attributes = convert_omop_to_cadence_csv.convert_omop_to_cadence(
-		    conn,
-		    generate_query_csv.attributes(
-		        person_csv_path=person_table_csv_path,
-		        concept_csv_path=concept_table_csv_path,
-		        year_for_age_calculation=year_for_age_calculation
-		    )
-		).df()
+		if datasource == 'CSV':
+			df_attributes = convert_omop_to_cadence_csv.convert_omop_to_cadence(
+			    conn,
+			    generate_query_csv.attributes(
+			        person_csv_path=person_table_csv_path,
+			        concept_csv_path=concept_table_csv_path,
+			        year_for_age_calculation=year_for_age_calculation
+			    )
+			).df()
+		if datasource == 'PostgreSQL':
+			cur.execute('CREATE SCHEMA IF NOT EXISTS cadence')
+			cur.execute('DROP TABLE IF EXISTS cadence.attributes')
+			cur.execute('DROP TABLE IF EXISTS cadence.events')
+			cur.execute(
+			    generate_query_postgres.attributes(
+			        person_table=person_table_postgres,
+			        concept_table=concept_table_postgres,
+			        year_for_age_calculation=year_for_age_calculation
+			    )
+			)
+			conn.commit()
+			cur.execute('SELECT * FROM cadence.attributes')
+			df_attributes = pd.DataFrame(cur.fetchall())
+			df_attributes.columns = [desc[0] for desc in cur.description]
 
 		st.dataframe(df_attributes.head(20))
 
@@ -132,13 +198,26 @@ with st.form("config"):
 
 		# CADENCE Events Table
 		st.subheader('Events Table (sample)')
-		df_events = convert_omop_to_cadence_csv.convert_omop_to_cadence(
-		    conn,
-		    generate_query_csv.events(
-		        condition_occurrence_csv_path=condition_occurrence_table_csv_path,
-		        concept_csv_path=concept_table_csv_path
-		    )
-		).df()
+		if datasource == 'CSV':
+			df_events = convert_omop_to_cadence_csv.convert_omop_to_cadence(
+			    conn,
+			    generate_query_csv.events(
+			        condition_occurrence_csv_path=condition_occurrence_table_csv_path,
+			        concept_csv_path=concept_table_csv_path
+			    )
+			).df()
+		if datasource == 'PostgreSQL':
+			cur.execute('DROP TABLE IF EXISTS cadence.events')
+			cur.execute(
+			    generate_query_postgres.events(
+			        condition_occurrence_table=condition_occurrence_table_postgres,
+			        concept_table=concept_table_postgres
+			    )
+			)
+			conn.commit()
+			cur.execute('SELECT * FROM cadence.events')
+			df_events = pd.DataFrame(cur.fetchall())
+			df_events.columns = [desc[0] for desc in cur.description]
 
 		st.dataframe(df_events.head(20))
 
@@ -182,22 +261,29 @@ with st.form("config"):
 		    use_container_width=True
 		)
 
-		convert_omop_to_cadence_csv.if_dir_not_exists_create(csv_export_path)
-		convert_omop_to_cadence_csv.export_as_csv(
-		    conn,
-		    generate_query_csv.attributes(
-		        person_csv_path=person_table_csv_path,
-		        concept_csv_path=concept_table_csv_path,
-		        year_for_age_calculation=year_for_age_calculation
-		    ), f"{csv_export_path}/attributes.csv"
-		)
+		if datasource == 'CSV':
+			convert_omop_to_cadence_csv.if_dir_not_exists_create(csv_export_path)
+			convert_omop_to_cadence_csv.export_as_csv(
+			    conn,
+			    generate_query_csv.attributes(
+			        person_csv_path=person_table_csv_path,
+			        concept_csv_path=concept_table_csv_path,
+			        year_for_age_calculation=year_for_age_calculation
+			    ), f"{csv_export_path}/attributes.csv"
+			)
 
-		convert_omop_to_cadence_csv.export_as_csv(
-		    conn,
-		    generate_query_csv.events(
-		        condition_occurrence_csv_path=condition_occurrence_table_csv_path,
-		        concept_csv_path=concept_table_csv_path
-		    ), f"{csv_export_path}/events.csv"
-		)
+			convert_omop_to_cadence_csv.export_as_csv(
+			    conn,
+			    generate_query_csv.events(
+			        condition_occurrence_csv_path=condition_occurrence_table_csv_path,
+			        concept_csv_path=concept_table_csv_path
+			    ), f"{csv_export_path}/events.csv"
+			)
 
-		st.success(f'Conversion completed! Exported to {csv_export_path}')
+			st.success(f'Conversion completed! Exported to {csv_export_path}')
+
+		if datasource == 'PostgreSQL':
+			cur.close()
+			conn.close()
+
+			st.success(f'Conversion completed! Exported to `cadence.attributes` and `cadence.events`.')
